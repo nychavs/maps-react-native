d1e37ae9756672dbdcbab35e08e0db3f08974fd5
nychavs <chavesnaiury@gmail.com>
2023-03-03 09:07:00 -0300

    list of markers

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -5,24 +5,44 @@ import {Text, View} from "../components/Themed";
 
 export default function MapScreen() {
 
-  const [latitude, setLatitude] = useState(-22.91387958710525)
-  const [longitude, setLongitude] = useState(-47.068131631428884)
+  const [lat, setLatitude] = useState(-22.91387958710525)
+  const [long, setLongitude] = useState(-47.068131631428884)
+  const locals = [{
+    latitude: -22.896875688928752,
+    longitude: -47.11146552703074,
+    title: 'bosch'},
+    {latitude: -22.91387958710525,
+    longitude: -47.068131631428884,
+    title: 'senai'},
+    {latitude: -22.850590745952736,
+    longitude:  -47.231195788477336,
+    title: 'ifsp'},
+    {latitude:-22.858229699890725,
+    longitude: -47.20148198835639,
+    title: 'casa'},
+    {latitude: -22.85520918203911,
+    longitude:  -47.224505384778055,
+    title: 'academia'},
 
+  ]
   return (
+    
     <View style={styles.container}>
       <MapView style={styles.map}
         initialRegion={{
-          latitude: latitude,
-          longitude: longitude,
+          latitude: lat,
+          longitude: long,
           latitudeDelta: 0.003,
           longitudeDelta: 0.003,
         }}> 
+      {locals.map((local) => (
+        <Marker 
+        key={local.title}
+        coordinate={{latitude: local.latitude, longitude: local.longitude}}
+        title={local.title}>
+        </Marker>
+      ))}
 
-      <Marker coordinate={{ latitude : latitude, longitude : longitude }}>
-        <Callout>
-          <Text>SENAI</Text>
-        </Callout>
-      </Marker>
       </MapView>
     </View>
   );
